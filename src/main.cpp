ca899b42c596e89ddc87522861376de3d9b06756
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-10-16 18:32:27 +0200

    Add CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::puts ("Hello magister!");
     std::cout << __cplusplus << "\n";
-    Logger::makeLogEntry("Hello magister :)");
+    //Logger::makeLogEntry("Hello magister :)");
 
     return 0;
 }
