d8aa6e3075326fde8760ea57feaca9d535a281be
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-10-20 19:02:57 +0200

    Add test for Sentence.cpp

--- tests/dummy/dummyTest.cpp
+++ tests/dummy/dummyTest.cpp
@@ -1,8 +1,14 @@
 #include <gtest/gtest.h>
+#include "Sentence.hpp"
 
 TEST(Test, cppVersion) {
     ASSERT_TRUE(42 > 24);
 }
+
+TEST(Test, dummy01) {
+    ASSERT_TRUE(1.0f == makeMeasure("foo", "foo"));
+}
+
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
